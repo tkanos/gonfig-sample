80ab59328b7ea4cfba676ab022685bd2d963a98c
administrator <tkanos@gmail.com>
2017-01-04 21:38:27 +0100

    updating code to follow gonfig

--- main.go
+++ main.go
@@ -16,19 +16,19 @@ type Configuration struct {
 
 func main() {
 
+	// mock env variable
 	os.Setenv("Connection_String", "test")
 	os.Setenv("Port", "8081")
 
 	configuration := Configuration{}
-	err := gonfig.GetJsonConfiguration(getFileName(), &configuration)
+	err := gonfig.GetConf(getFileName(), &configuration)
 	if err != nil {
 		os.Exit(500)
 	}
 
-	gonfig.GetEnvVariablesConfiguration(&configuration)
-
 	fmt.Println(configuration.Port)
 	fmt.Println(configuration.Connection_String)
+
 }
 
 func getFileName() string {
